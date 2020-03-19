503a80d714e4e020b4b6cf9cceb312ae7b3ac602
Andrey Popov <Andrey.Popov@gidi.it>
2020-03-19 15:11:30 +0300

    form_make

---
+++ tc_tester/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tc_tester
+{
+    static class Program
+    {
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            if (!connectionStatus)
+            {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            }
+            
+        }
+        public static void getIp(String ip)
+        {
+            if (string.IsNullOrWhiteSpace(ip))
+            {
+                throw new ArgumentException("Ip can't be empty.", nameof(ip));
+            } else
+            {
+                
+            }
+   
+        }
+
+        private static bool connectionStatus;
+
+        public static bool GetConnectionStatus()
+        {
+            return connectionStatus;
+        }
+
+        public static void SetConnectionStatus(bool value)
+        {
+            connectionStatus = value;
+        }
+
+    }
+}
